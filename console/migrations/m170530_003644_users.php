dc8e028e698bf83c8d48dca085b451a59cc50848
Evan <evan@err7.net>
2017-05-30 09:20:31 +0800

    Add migration users

--- console/migrations/m170530_003644_users.php
+++ console/migrations/m170530_003644_users.php
@@ -2,16 +2,11 @@
 
 use yii\db\Migration;
 
-class m130524_201442_init extends Migration
+class m170530_003644_users extends Migration
 {
+    public $tableName = '{{%users}}';
     public function up()
     {
-        $tableOptions = null;
-        if ($this->db->driverName === 'mysql') {
-            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
-        }
-
         $this->createTable('{{%user}}', [
             'id' => $this->primaryKey(),
             'username' => $this->string()->notNull()->unique(),
@@ -19,15 +14,28 @@ class m130524_201442_init extends Migration
             'password_hash' => $this->string()->notNull(),
             'password_reset_token' => $this->string()->unique(),
             'email' => $this->string()->notNull()->unique(),
-
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
-        ], $tableOptions);
+        ]);
+
     }
 
     public function down()
     {
-        $this->dropTable('{{%user}}');
+        echo "m170530_003644_users cannot be reverted.\n";
+
+        return false;
     }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
 }
