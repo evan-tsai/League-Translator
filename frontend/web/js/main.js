1bfe3002d865504f66e1392d50adabc1fe57a9be
Evan Tsai <evan@err7.net>
2017-06-07 16:10:56 +0800

    Fix Ajax URL

--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -1,7 +1,7 @@
 $(function(){
    $(document).on('change', '#language', function() {
        var lang = $('#language :selected').val();
-       $.post('site/language', {'lang':lang}, function(data) {
+       $.post('/site/language', {'lang':lang}, function(data) {
            location.reload();
        });
    });
